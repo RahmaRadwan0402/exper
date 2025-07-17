14249c45d45a3a30eb71c19eefeb05827d049d4b
abdulllah ali <engineerabdallah67@gmail.com>
2025-07-17 16:56:12 +0300

    EditOnPersistLogin

--- src/Pages/Auth/PresistLogin.js
+++ src/Pages/Auth/PresistLogin.js
@@ -1,56 +1,56 @@
-import Cookies from 'universal-cookie'
+import Cookies from "universal-cookie";
 import { useContext, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { User } from "../Context/Usercontext";
-import LoadingPage from "../../components/website/LoadingPage"
-import axios from 'axios';
+import LoadingPage from "../../components/website/LoadingPage";
+import axios from "axios";
 
- 
+export default function PersistLogin() {
+  // Get current User
+  const context = useContext(User);
+  const token = context.auth.token;
+  const [loading, setLoading] = useState(true);
 
-export default function Presistlogin(){
+  // cookie
+  const cookie = new Cookies();
 
-   // Get current User
-   const context = useContext(User);
-  const token = context.auth.token
-  const [loading, setLoading] = useState(true)
-
-
-// cookie
-const cookie = new Cookies();
-const getToken = cookie.get("Bearer")
-console.log(getToken)
-
-
-
-
-// Send Refresh token
-
-useEffect(() => {
-async function refresh() {
-  try{
-    await axios
-    .post(`http://127.0.0.1:8000/api/refresh`, null,
-      {headers:{
-      Authorization:"Bearer " + getToken,
-    },
-  })
-      .then((data) => {
-        console.log(data)
-        cookie.set("Bearer", data.data.token);
+  useEffect(() => {
+    async function refresh() {
+      try {
+        const authData = cookie.get("authData");
+        if (!authData) {
+          setLoading(false);
+          return;
+        }
+        const { data } = await axios.post(
+          `http://127.0.0.1:8000/api/refresh`,
+          null,
+          {
+            headers: {
+              Authorization: "Bearer " + authData.token,
+            },
+          }
+        );
+        cookie.set("authData", {
+          userDetails: data?.user,
+          token: data?.token,
+        });
         context.setAuth((prev) => {
-          return{userDetails: data.data.User, token: data.data.token}})
-  });
-    
-  }
-  catch(err){
-    console.log(err)
-  }finally{
-    setLoading(false)
-  }
-}
+          return { userDetails: data.user, token: data.token };
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
 
-!token ? refresh() : setLoading(false)
-}, []);
-    return loading ?  <LoadingPage /> : <Outlet />
-    
+    if (!token) {
+      refresh();
+    } else {
+      setLoading(false);
+    }
+  }, [token, context]);
+
+  return loading ? <LoadingPage /> : <Outlet />;
 }
